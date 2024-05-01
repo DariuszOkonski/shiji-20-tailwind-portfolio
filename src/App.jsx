b261b07e50b7404e833ada02413782dd78a071e2
Dariusz Okoński <darek200180@gmail.com>
2024-05-01 12:19:07 +0200

    add Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+      <Hero />
     </React.Fragment>
   );
 }
